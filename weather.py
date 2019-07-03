79eb4fedf424516fa1b32e159780123681d8985d
abdullah998 <52492005+abdullah998@users.noreply.github.com>
2019-07-03 19:24:36 +0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,24 +1,67 @@
 import json
 import requests
-def getWeather(c):
-    id = "f331581e1725a93c28647659d1afcc02"
 
-    api = 'http://api.openweathermap.org/data/2.5/weather'
-    r = requests.get(url=api, params=dict(q=c, APPID=id))
-    return r
-id="f331581e1725a93c28647659d1afcc02"
 
-api = 'http://api.openweathermap.org/data/2.5/weather'
-cities=["Attock","Bahawalnagar","Bahawalpur","Bhakkar","Chakwal","Chiniot","Faisalabad","Gujranwala","Gujrat","Hafizabad",
-        "Jhang","Jhelum","Kasur","Khanewal","Khushab","Lahore","Layyah","Lodhran","Mandi Bahauddin","Multan"]
-d={}
-print('Fetching results...')
-for c in cities:
-    r=getWeather(c)
-    obj=json.loads(r.text)
-    d[obj['name']]=obj
-for k in sorted(d,key =lambda name: d[name]['main']['temp']):
-    print(k,end='\t')
-    print(d[k]['main']['temp']-273)
+CITIES = [
+    "Attock",
+    "Bahawalnagar",
+    "Bahawalpur",
+    "Bhakkar",
+    "Chakwal",
+    "Chiniot",
+    "Faisalabad",
+    "Gujranwala",
+    "Gujrat",
+    "Hafizabad",
+    "Jhang",
+    "Jhelum",
+    "Kasur",
+    "Khanewal",
+    "Khushab",
+    "Lahore",
+    "Layyah",
+    "Lodhran",
+    "Mandi Bahauddin",
+    "Multan",
+]
 
-    
+
+def get_weather(c):
+    identifier = "f331581e1725a93c28647659d1afcc02"
+
+    api = "http://api.openweathermap.org/data/2.5/weather"
+    response = requests.get(url=api, params=dict(q=c, APPID=identifier))
+    return response
+
+
+def fetch_weather(cities):
+    dictionary = {}
+
+    print("Fetching results...")
+    for city in cities:
+        result = get_weather(city)
+        obj = json.loads(result.text)
+        dictionary[obj["name"]] = obj
+    return dictionary
+
+
+def sort_by_temperature(dictionary):
+    cities=[]
+    for city_name in sorted(dictionary, key=lambda name: dictionary[name]["main"]["temp"]):
+        cities.append((city_name,dictionary[city_name]["main"]["temp"]))
+    return cities
+
+
+def kelvin_to_centi(temp):
+    return temp-273
+
+
+def print_cities(SORTED_CITIES_LIST):
+    for name, temperature in SORTED_CITIES_LIST:
+        print(name, end="\t")
+        print(kelvin_to_centi(temperature))
+
+
+dictionary = fetch_weather(CITIES)
+SORTED_CITIES_LIST = sort_by_temperature(dictionary)
+print_cities(SORTED_CITIES_LIST)
